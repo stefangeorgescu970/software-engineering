eac99e210994d0e425d724f472855dd30ec02301
Unknown <stefan.georgescu.970@gmail.com>
2017-11-07 22:18:10 +0100

    proj init

---
+++ theGame/Server/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Server
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
