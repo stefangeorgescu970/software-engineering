9d8d148612e9feb17a8f4543d8ba597dad2b042a
Unknown <stefan.georgescu.970@gmail.com>
2017-11-07 23:33:25 +0100

    first contact
    The two entities can communicate

--- theGame/Client/Client.cs
+++ theGame/Client/Client.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Net.Sockets;
 using System.Net;
+using System.Text;
 
 namespace Client
 {
@@ -8,22 +9,25 @@ namespace Client
 
         private static Socket _mySocket = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
 
-        private static void LoopConnect(){
+        private static void loopConnect(){
 
 
             int attempts = 0;
 
             while (!_mySocket.Connected){
 
-                try
-                {
+                try{
+                    
+                    System.Threading.Thread.Sleep(2000);
                     attempts++;
                     _mySocket.Connect(IPAddress.Loopback, 8080);
 
+
                 }
-                catch (SocketException) 
-                {
+                catch (SocketException) {
+                    Console.Clear();
                     Console.WriteLine("Connection Attempts: " + attempts);
+
                 }
             }
 
@@ -32,9 +36,32 @@ namespace Client
         }
 
 
+        private static void sendLoop(){
+
+            while(true) {
+                Console.Write("Enter Message: ");
+                string text = Console.ReadLine();
+
+                byte[] toSend = Encoding.ASCII.GetBytes(text);
+                _mySocket.Send(toSend);
+
+                byte[] receivedBuffer = new byte[2048];
+                int sizeReceived = _mySocket.Receive(receivedBuffer);
+
+                byte[] actualData = new byte[sizeReceived];
+                Array.Copy(receivedBuffer, actualData, sizeReceived);
+
+                Console.WriteLine("Received: " + Encoding.ASCII.GetString(actualData));
+            }
+
+
+        }
+
         public static void Main(string[] args)
         {
-            LoopConnect();
+            loopConnect();
+
+            sendLoop();
 
             Console.ReadLine();
         }
