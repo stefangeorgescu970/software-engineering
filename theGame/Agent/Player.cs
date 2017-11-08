df6f5e9c459bc1b6e54226616963f24a298c402b
tareqhabbab <tareqhabbab@github.com>
2017-11-08 22:37:21 +0100

    Agent added

---
+++ theGame/Agent/Player.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agent
+{
+    class Player
+    {
+        Tuple<int, int> position;
+        Team myTeam;
+        public Player(int i, int j)
+        {
+            position = new Tuple<int, int>(i, j);
+            myTeam = null;
+        }
+        public void setTeam(Team myTeam)
+        {
+            this.myTeam = myTeam;
+        }
+
+
+        /* return a pair of the new position
+         * the method should test every possible position whether it is ocupied by other player or not, by asking the server about it
+        */
+        public Tuple<int, int> Move()   
+        {
+            int []dx = {-1, 0, 1, 0};
+            int []dy = {0, 1, 0, -1};
+            Random r = new Random();
+            while (true)
+            {
+                int idx = r.Next(4);
+                int newX = position.Item1 + dx[idx];
+                int newY = position.Item2 + dy[idx];
+                if (newX >= 0 && newX < 10 && newY >= 0 && newY < 10/* && (newX, newY) are not ocupied */ )   // 10 should subtituted by the board size
+                    return new Tuple<int, int>(newX, newY);
+            }
+        }
+
+
+    }
+}
