1dbf5eccaa0049ee7ac7a211f16c5df2d0e27764
tareqhabbab <tareqhabbab@github.com>
2017-11-11 23:28:04 +0100

    Player edited

--- theGame/Agent/Program.cs
+++ theGame/Agent/Program.cs
@@ -10,7 +10,11 @@ namespace Agent
     {
         static void Main(string[] args)
         {
-
+            List<int> a = new List<int>();
+            a.Add(1);
+            a.Add(2);
+            a.Add(3);
+           
         }
     }
 }
